5a8081f3d1ba3338483f69fa417ae13512979a16
Peter Landoll <pwlandoll@gmail.com>
2020-09-12 10:35:17 -0400

    Add proper pin matrix

--- pwlandoll/config.h
+++ pwlandoll/config.h
@@ -31,9 +31,11 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define MATRIX_ROWS 4
 #define MATRIX_COLS 12
 
-#define MATRIX_ROW_PINS { C6, D4, D0, D1 }
-#define MATRIX_COL_PINS { E6, D7, B5, B4, F6, F7, F4, F5, B1, B3, B2, B6 }
-#define UNUSED_PINS
+// Row pins on A0, A1, A2, A3
+#define MATRIX_ROW_PINS { F7, F6, F5, F4 }
+// Column pins on A4, A5, 0, 1, 13, 12, 11, 10, 9, 6, 5, 3
+#define MATRIX_COL_PINS { F1, F0, D2, D3, C7, D6, B7, B6, B5, D7, C6, D0 }
+#define UNUSED_PINS { D1 }
 
 /* COL2ROW, ROW2COL*/
 #define DIODE_DIRECTION COL2ROW
